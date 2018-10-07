fe901f4d22384eefb3a6ff4ee9036f7cf38ea200
Макс Сухарев <mazy_max@mail.ru>
2018-10-08 01:06:49 +0800

    Добавьте файлы проекта.

---
+++ WebApiApplication/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+using WebApiApplication.Models;
+
+namespace WebApiApplication.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        Model m = new Model();
+       string tmp;
+
+        //GET
+        public IEnumerable<Record> Get()
+        {
+            return m.GetAllRecords();
+        }
+
+        // GET api/values/5
+        public Record Get(int id)
+        {
+            return m.GetRecordWithId(id);
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            tmp = value;
+            
+        }
+
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApiApplication/Models/Model.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApiApplication.Models
+{
+    public class Model
+    {
+        public string ConnectionInfo { get; set; }
+
+        public string ConnectionString { get; }
+
+        public Model()
+        {
+            ConnectionString = ConfigurationManager.ConnectionStrings["RecordsConnection"].ConnectionString;
+            
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                ConnectionInfo = connection.Database + " - " + connection.State; 
+            }
+            
+        }
+        
+        /// <summary>
+        ////Метод считывает все записи из таблицы Records
+        /// </summary>
+        public List<Record> GetAllRecords()
+        {
+            List<Record> records = new List<Record>();
+            string sqlExpression = "SELECT * FROM Records";
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows) 
+                {
+                    
+                    while (reader.Read()) // построчно считываем данные
+                    {
+                        Record rec = new Record
+                        {
+                            Id = (int)reader.GetValue(0),
+                            Code = (int)reader.GetValue(1),
+                            Value = reader.GetValue(2).ToString()
+                        };
+                        records.Add(rec);
+                    }
+                }
+                reader.Close();
+            }
+            return records;
+        }
+
+        /// <summary>
+        /// Метод считывает из базы запись с конкретным Id. 
+        /// Если запись в базе не найдена, то возвращает пустую запись.
+        /// </summary>
+        public Record GetRecordWithId(int Id)
+        {
+            string sqlExpression = "SELECT * FROM Records WHERE Id=" + Id.ToString();
+
+            Record rec = new Record();
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    rec.Id = (int)reader.GetValue(0);
+                    rec.Code = (int)reader.GetValue(1);
+                    rec.Value = reader.GetValue(2).ToString();
+                }
+                reader.Close();
+            }
+            return rec;
+        }
+
+
+    }
+}
